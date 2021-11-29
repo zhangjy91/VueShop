7c92172a20cf7eadd0873aac0dbb4e1c38a124ce
jy91 <zjyzjy56@163.com>
2021-11-30 02:15:45 +0800

    models_goods

--- VueShop/settings.py
+++ VueShop/settings.py
@@ -44,6 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DjangoUeditor',
+    'goods',
+    'users',
+    'trade',
+    'user_operation'
 ]
 
 MIDDLEWARE = [
